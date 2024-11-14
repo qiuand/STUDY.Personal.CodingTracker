c6e27eb5c4084cce573343f7558291b43395d120
Andrew Qiu <andrew.qiu@stickyricegames.com>
2024-11-14 16:27:40 -0500

    Unit 2

---
+++ CSharpProjects/TestProject/Program.cs
@@ -0,0 +1,16 @@
+﻿string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+
+foreach (char i in charMessage)
+{
+    if (i == 'o') { x++; }
+}
+
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
